9c0c6f33896e068565bc4179cbe6ba988679e08e
keitqr <kat.univ96@gmail.com>
2023-06-23 14:19:27 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -12,48 +12,6 @@ import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 
 
-
-
-
-//Communicating with Clarifai API: PAT. USER_ID given in their documentantion, and website.
-const returnClarifaiRequestOptions = (imageUrl) => {
-    const PAT = '4b3e491c65e64af4bcd78a4c6656bb52';
-    const USER_ID = 'keittaki';       
-    const APP_ID = 'face_reco';
-    // Change these to whatever model and image URL you want to use
-    // const MODEL_ID = 'face-detection';  
-    const IMAGE_URL = imageUrl;
-
-    const raw = JSON.stringify({
-      "user_app_id": {
-          "user_id": USER_ID,
-          "app_id": APP_ID
-      },
-      "inputs": [
-          {
-              "data": {
-                  "image": {
-                      "url": IMAGE_URL
-                  }
-              }
-          }
-      ]
-  });
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Authorization': 'Key ' + PAT
-    },
-    body: raw
-  }; 
- 
-  return requestOptions;  //so this is going to return for us the method headers and the body which will have the image URL.
-
-}
- 
-
 const initialState = {
   input: '',
   imageUrl: '', // We want to have an 'imageUrl' state and it will be empty for now and this 'imageUrl' should
@@ -90,20 +48,25 @@ class App extends Component {
   }
 
 
-
-  calculateFaceLocation = (data) => { //creating a function which accepts parameteres of the data of the face location 
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return { //what is going to fill up the box state above with the values bellow of the face, the coordinates of the boxs ends
+  calculateFaceLocation = (data) => {
+    try {
+      const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+      const image = document.getElementById('inputimage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+  
+      return {
         topRow: clarifaiFace.top_row * height,
-        rightCol: width - (clarifaiFace.right_col * width ),
-        bottomRow: height - (clarifaiFace.bottom_row * height),
+        rightCol: width - clarifaiFace.right_col * width,
+        bottomRow: height - clarifaiFace.bottom_row * height,
         leftCol: clarifaiFace.left_col * width
+      };
+    } catch (error) {
+      console.log('Error in calculateFaceLocation:', error);
+      return {}; // Return an empty object or handle the error appropriately
     }
-
   }
+  
 
 
 
@@ -118,36 +81,36 @@ class App extends Component {
   }  //we want to pass it as a prop to the linkform
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});  //to have image url updated with whatever the 'input' is.
- 
-    fetch(
-      "https://api.clarifai.com/v2/models/" 
-      + 'face-detection' 
-      + "/outputs", 
-      returnClarifaiRequestOptions(this.state.input))
-      .then(response => response.json())  
+    this.setState({ imageUrl: this.state.input });
+  
+    fetch("http://localhost:3000/imageurl", {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
       .then(response => {
-        if (response){
+        if (response) {
           fetch("http://localhost:3000/image", {
             method: 'put',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-              id: this.state.user.id})   
+              id: this.state.user.id
             })
+          })
             .then(response => response.json())
             .then(count => {
-              this.setState(Object.assign(this.state.user, {entries: count}))
+              this.setState(Object.assign(this.state.user, { entries: count }))
             })
-            .catch(console.log)
-        
-      
+            .catch(console.log);
         }
-        this.displayFaceBox(this.calculateFaceLocation(response)) 
-      }) // this gives us the coordinations of the face box.
-      // .then(response => console.log(this.calculateFaceLocation(response)))
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
-      
-    }  
+  }
+  
 
 
     onRouteChange = (route) => {
